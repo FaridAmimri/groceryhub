17be874cfde815e9e2754a2f99002aa1b7088745
Farid Amimri <farid.amimri@gmail.com>
2024-04-16 18:15:24 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import type { Metadata } from 'next'
+import { Outfit } from 'next/font/google'
+import './globals.css'
+
+const outfit = Outfit({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Grocery Hub',
+  description: 'Buy your fruits and vegetables'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={outfit.className}>{children}</body>
+    </html>
+  )
+}
