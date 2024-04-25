a3913d6680ee24bced2d5efea520d55e16c3c7c1
Farid Amimri <farid.amimri@gmail.com>
2024-04-25 12:39:31 +0200

    Add ProductList Api Endpoint

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,14 +1,28 @@
 /** @format */
 
 import { prisma } from '@/utils/connect'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 
-// FETCH ALL PRODUCTS
-export const GET = async () => {
+// FETCH ALL PRODUCTS BY CATEGORY OR WITHOUT
+export const GET = async (req: NextRequest) => {
+    const { searchParams } = new URL(req.url)
+    const cat = searchParams.get('cat')
+
     try {
-        const products = await prisma.product.findMany()
-        return new NextResponse(JSON.stringify(products), { status: 200 })
+        if (cat) {
+            const products = await prisma.product.findMany({
+                where: {
+                    ...cat && { catSlug: cat }
+                }
+            })
+            return new NextResponse(JSON.stringify(products), { status: 200 })
+        }
+        else {
+            const products = await prisma.product.findMany()
+            return new NextResponse(JSON.stringify(products), { status: 200 })
+        }
+
     } catch (error) {
         console.log(error)
         return new NextResponse(
