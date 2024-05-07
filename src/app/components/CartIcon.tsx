158950e32f94ad4c2e4146535abb6821f0b376e5
Farid Amimri <farid.amimri@gmail.com>
2024-05-07 15:08:16 +0200

    Update Cart Items display using sheet shadcn/ui

--- src/app/components/CartIcon.tsx
+++ src/app/components/CartIcon.tsx
@@ -3,17 +3,90 @@
 'use client'
 
 import { useCartStore } from '@/utils/store'
-import Link from 'next/link'
-import { ShoppingBag } from 'lucide-react'
+import { ShoppingBasket, TrashIcon } from 'lucide-react'
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+  SheetClose
+} from '@/components/ui/sheet'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const CartIcon = () => {
-  const { totalItems } = useCartStore()
+  const { products, totalItems, totalPrice, removeFromCart } = useCartStore()
+  const { data: session } = useSession()
+  const router = useRouter()
 
   return (
-    <Link href='/cart' className='flex gap-2 items-center text-lg'>
-      <ShoppingBag />
-      <span>{totalItems}</span>
-    </Link>
+    <Sheet>
+      <SheetTrigger>
+        <h2 className='flex gap-2 items-center text-lg'>
+          <ShoppingBasket />
+          <span className='bg-primary text-white  px-2 rounded-full'>
+            {totalItems}
+          </span>
+        </h2>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle
+            className='bg-primary
+                text-white font-bold text-lg p-2 w-[90%] self-center'
+          >
+            My Cart
+          </SheetTitle>
+          <SheetDescription>
+            <div className='h-[500px] overflow-auto'>
+              {products.map((product) => (
+                <div
+                  key={product.id}
+                  className='flex justify-between items-center p-2 mb-5'
+                >
+                  <div className='flex gap-6 items-center'>
+                    <Image
+                      src={product.img!}
+                      width={90}
+                      height={90}
+                      alt={product.title}
+                      className='border p-2'
+                    />
+                    <div>
+                      <h2 className='font-bold'>{product.title}</h2>
+                      <h2 className=''>Quantity {product.quantity}</h2>
+                      <h2 className='text-lg font-bold'>{product.price} €</h2>
+                    </div>
+                  </div>
+                  <TrashIcon
+                    className='cursor-pointer'
+                    onClick={() => removeFromCart(product)}
+                  />
+                </div>
+              ))}
+            </div>
+          </SheetDescription>
+        </SheetHeader>
+        <SheetClose asChild>
+          <div className='absolute w-[90%] bottom-6 flex flex-col bg-white'>
+            <h2 className='text-lg font-bold flex justify-between'>
+              Subtotal
+              <span>{totalPrice} €</span>
+            </h2>
+            <Button
+              disabled={products.length === 0}
+              onClick={() => router.push(session ? '/' : '/sign-in')}
+            >
+              Checkout
+            </Button>
+          </div>
+        </SheetClose>
+      </SheetContent>
+    </Sheet>
   )
 }
 
